d9ff668142dcb9a129808798dbf96e4de5fc4831
keithzg <keithzg@gmail.com>
2016-12-25 15:58:13 -0700

    Semi-working implementation of WallCalendar (still set to 2015, still largely Russian, and still doesn't fully compile)

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Well, I read http://www.metronews.ca/news/edmonton/2016/12/23/edmontonian-makes-
 Pshhh. I think it's pretty clear that even if the satirical nature of this isn't enough to qualify for a defense against charges of infringement on satirical grounds, that this keeps happening would seem to make clear that this would fall under the doctrine of Fair Dealing for educational purposes alone!
 
 Still, it's best to keep licenses and legalities clear, and there may well be room for creating an Edmonton calendar where one might want to swap out other images instead. So my intention here is to re-use some sort of [Free](https://www.gnu.org/philosophy/free-sw.en.html) calendar template, and then a script to download appropriate images. 
+
+I'm re-using the [WallCalendar](https://github.com/vitalyrepin/WallCalendar) project, which seems like it'll work decently, and there's also something appropriate at re-using a Russian project for an Edmontonian project, no?
+
+## Dependencies
+ latexmk texlive-latex-recommended texlive-latex-extra texlive-luatex texlive-fonts-extra
+
+Dependencies I should remove:
+ texlive-lang-cyrillic
---
+++ WallCalendar/gather_images.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+r="resources/HighLevel"
+
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/jan.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/feb.png
+# http://globalnews.ca/news/2579971/semi-wedged-underneath-overpass-near-alberta-legislature/
+curl "https://shawglobalnews.files.wordpress.com/2016/03/img_3375.jpg?quality=70&strip=all&w=1200&h=800&crop=1" -o ${r}/mar.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/apr.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/may.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/jun.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/jul.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/aug.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/sep.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/oct.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/nov.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/dec.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/front.png
+# From https://twitter.com/yegtraffic/status/289108067793113088
+curl https://pbs.twimg.com/media/BAMeGoGCAAAFsDm.jpg:large -o ${r}/back.png
